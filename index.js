4e84f88ff4e5f06eec3d97719df357b7514d628f
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-06 22:11:26 +0900

    introduce mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const passportConfig = require('./services/passport');
+const keys = require('./config/keys');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);  
+app.listen(PORT);
