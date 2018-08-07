e490c1ddaa426c89f1476fc9184ae760c99dc959
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-08 00:16:48 +0900

    add deserialize userID function

--- services/passport.js
+++ services/passport.js
@@ -7,7 +7,14 @@ const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
     done(null, user,id);
-})
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        });
+});
 
 passport.use(new GoogleStrategy(
     {
