991bfc366fc87bb794c79a8191fffb4f1905755b
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-10 18:01:03 +0900

    add routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from "./Header";
 
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
-const survyeNew = () => <h2>SurveyNew</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 
@@ -12,7 +12,10 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path = "/" component = { Landing } />
+                    <Header />
+                    <Route exact path = "/" component = { Landing } />
+                    <Route exact path = "/surveys" component = { Dashboard } />
+                    <Route path = "/surveys/new" component = { SurveyNew } />
                 </div>
             </BrowserRouter>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import App from "./components/App";
 
