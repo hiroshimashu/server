396d73e752d154b45e3c784d1751b5a95914ff40
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-10 19:30:08 +0900

    add authReducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from "./Header";
+import { connect } from 'react-redux';
+import * as actions from "../actions";
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path = "/" component = { Landing } />
-                    <Route exact path = "/surveys" component = { Dashboard } />
-                    <Route path = "/surveys/new" component = { SurveyNew } />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+    render() {
+        return(
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path = "/" component = { Landing } />
+                        <Route exact path = "/surveys" component = { Dashboard } />
+                        <Route path = "/surveys/new" component = { SurveyNew } />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+
+export default connect(null, actions)(App);
