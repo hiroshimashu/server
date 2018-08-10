5f733840bea60bf9dc12232d5c02da055c09e625
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-10 21:00:59 +0900

    connect backend authentication to frontend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 class Header extends Component {
+
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href = "/auth/google">
+                            Login with google
+                        </a>
+                    </li>
+                )
+            default: 
+                return (
+                <li>
+                    <a>
+                        Logout
+                    </a>
+                </li>
+                )
+
+        }
+    }
+
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className = "nav-wrapper">
@@ -9,11 +36,7 @@ class Header extends Component {
                         Emaily
                     </a>
                     <ul className =  "right">
-                        <li>
-                            <a>
-                                Login with Google
-                            </a>
-                        </li>
+                        { this.renderContent() }
                     </ul>
                 </div>
             </nav>
@@ -21,4 +44,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
