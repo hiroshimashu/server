a68dccc329cddcba9994f7dc0de308f72402b203
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-12 20:40:35 +0900

    add login and logout routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -18,7 +19,7 @@ class Header extends Component {
             default: 
                 return (
                 <li>
-                    <a>
+                    <a href = "/api/logout">
                         Logout
                     </a>
                 </li>
@@ -32,9 +33,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className = "nav-wrapper">
-                    <a className = "left brand-logo">
+                    <Link 
+                        to = {this.props.auth ? '/surveys' : '/'} 
+                        className = "left brand-logo"
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className =  "right">
                         { this.renderContent() }
                     </ul>
