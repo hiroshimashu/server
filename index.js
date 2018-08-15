e0173593026b5335e464efc378d6edec20a50885
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-15 17:58:42 +0900

    Ready for production

--- index.js
+++ index.js
@@ -24,5 +24,19 @@ app.use(bodyParser.json());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === "production") {
+    // Express will server up produciton assets 
+    // like our main.js file, or main.css file!
+    app.use(exress.static('client/build'));
+
+    // Express will server up the index.html file 
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }) 
+
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
