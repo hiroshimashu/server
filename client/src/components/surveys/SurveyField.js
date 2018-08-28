ef02df9e163250be4910145a4361733abfc15d6a
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-28 17:29:31 +0900

    add validation to new survey form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,14 +1,17 @@
 // Survey field contains logic to render a single label and text input
 import React from 'react';
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched }}) => {
     return (
         <div>
             <label>
                 { label }
             </label>
             <div>
-                <input {...input} />
+                <input {...input} style = {{ marginBottom: '5px'}}/>
+                <div className = "red-text" style = {{ marginBottom: '20px'}}>
+                   { touched && error }
+                </div>
             </div>
         </div>
     )
